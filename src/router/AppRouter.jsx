38914ee9ebe1fc26e635da4cbc17f4acbaea74d6
aliaydogdu105 <aliaydogdu105@gmail.com>
2022-08-16 00:08:37 +0300

    Private router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,8 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GlobalStyles } from "../components/globalStyles/Global.styles";
 import Navbar from "../components/navbar/Navbar";
 import About from "../pages/about/About";
+import Detail from "../pages/detail/Detail";
 import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
   return (
@@ -13,7 +15,12 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
-        <Route path="about" element={<About />} />
+        <Route path="about" element={PrivateRouter}>
+          <Route path="" element={<About />} />
+        </Route>
+        <Route path="detail" element={PrivateRouter}>
+          <Route path="" element={<Detail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
