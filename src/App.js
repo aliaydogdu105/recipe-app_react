0263e860f798f70745996fdf4c29bbed3d326af4
aliaydogdu105 <aliaydogdu105@gmail.com>
2022-08-15 19:05:46 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import "./App.css";
+import AppRouter from "./router/AppRouter";
+import theme from "./components/globalStyles/theme";
+import { ThemeProvider } from "styled-components";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <AppRouter />
+    </ThemeProvider>
+  );
 }
 
 export default App;
